ca5dafe8717e8036337de10085b34c02de0d45b8
G2023 <qq754083503@gmail.com>
2023-02-15 12:23:34 +0800

    添加了递归函数

--- _001开始/源.cpp
+++ _001开始/源.cpp
@@ -1,7 +1,26 @@
 #include<stdio.h>
+long long str(int n)
+{
+	printf("%d\n", n);
+	if (n == 1 || n == 2)
+		return 1;
+	else
+		return str(n - 1) + str(n - 2);
+}
 int main()
 {
-	FILE* file;
+	/*FILE* file;
 	file = fopen("1.txt", "w");
+	return 0;*/
+	/*int k = 0;
+	for(int i=0;i<10;i++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			printf("%d", k);
+			k++;
+		}
+	}*/
+	printf("%lld\n", str(100));
 	return 0;
 }
